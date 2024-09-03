7ab78ddd94fc4f754d7f5c98936f74978286e8e3
adovgiy <adovgiy@zaoblako.ru>
2024-09-03 18:16:18 +0300

    Добавил отображение текст дня

--- Level.cs
+++ Level.cs
@@ -11,27 +11,22 @@ public partial class Level : Node2D
 	}
 	
 	private LightState _lightState = LightState.Morning;
+	private int _dayCount;
 	
 	private DirectionalLight2D _light;
 	private PointLight2D _pointLight;
+	private Label _dayLabel;
+	private AnimationPlayer _animationPlayer;
 
 	public override void _Ready() {
 		_pointLight = GetNode<PointLight2D>("PointLight2D");
+		_dayLabel = GetNode<Label>("CanvasLayer/DayLabel");
+		_animationPlayer = GetNode<AnimationPlayer>("CanvasLayer/AnimationPlayer");
 		_light = GetNode<DirectionalLight2D>("DirectionalLight2D");
 		_light.Enabled = true;
-	}
-
-	public override void _Process(double delta) {
-		switch (_lightState) {
-			case LightState.Morning: {
-				MorningState();
-				break;
-			}
-			case LightState.Evening: {
-				EveningState();
-				break;
-			}
-		}
+		_dayCount = 1;
+		SetDayText();
+		DayTextShow();
 	}
 
 	private void MorningState() {
@@ -51,11 +46,38 @@ public partial class Level : Node2D
 	}
 
 	private void _on_day_night_timeout() {
+		switch (_lightState) {
+			case LightState.Morning: {
+				MorningState();
+				break;
+			}
+			case LightState.Evening: {
+				EveningState();
+				break;
+			}
+		}
+		
 		if (_lightState == LightState.Night) {
 			_lightState = LightState.Morning;
+			_dayCount++;
+			SetDayText();
+			DayTextShow();
 		}
 		else {
 			_lightState += 1;
 		}
+	}
+
+	private void SetDayText() {
+		_dayLabel.Text = "Day " + _dayCount;
+	}
+
+	private void DayTextShow() {
+		_animationPlayer.Play("day_text_fade_in");
+		GetTree().CreateTimer(3f).Timeout += DayTextHide;
+	}
+
+	private void DayTextHide() {
+		_animationPlayer.Play("day_text_fade_out");
 	}
 }
