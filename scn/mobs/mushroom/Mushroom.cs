e55f7a71d0c7512f42d54298df99795493ccfebe
Aleksandr <aleksandr_dovgiyy@mail.ru>
2024-09-04 00:41:29 +0300

    Атака гриба

--- scn/mobs/mushroom/Mushroom.cs
+++ scn/mobs/mushroom/Mushroom.cs
@@ -2,10 +2,37 @@ using Godot;
 using System;
 
 public partial class Mushroom : CharacterBody2D {
+	public enum StateEnum {
+		Idle,
+		Attack,
+		Chase
+	}
+
+	private StateEnum _state = StateEnum.Idle;
+	private StateEnum State {
+		get => _state;
+		set
+		{
+			_state = value;
+			switch (value) {
+				case StateEnum.Idle: {
+					IdleState();
+					break;
+				}
+				case StateEnum.Attack: {
+					AttackState();
+					break;
+				}
+			}
+		}
+	}
+
 	private AnimationPlayer _animationPlayer;
+	private CollisionShape2D _attackRangeCollision;
 	
 	public override void _Ready() {
 		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_attackRangeCollision = GetNode<CollisionShape2D>("AttackDirection/AttackRange/CollisionShape2D");
 	}
 
 	public override void _PhysicsProcess(double delta) {
@@ -17,6 +44,27 @@ public partial class Mushroom : CharacterBody2D {
 	}
 
 	private void _on_attack_range_body_entered(Node2D body) {
+		State = StateEnum.Attack;
+	}
+
+	private void IdleState() {
+		_animationPlayer.Play("Idle");
+		GetTree().CreateTimer(1f).Timeout += IdleFinished;
+	}
+	
+	private void AttackState() {
 		_animationPlayer.Play("Attack");
+		_animationPlayer.AnimationFinished += AttackFinished;
+	}
+
+	private void AttackFinished(StringName name) {
+		_animationPlayer.AnimationFinished -= AttackFinished;
+		_attackRangeCollision.Disabled = true;
+		State = StateEnum.Idle;
+	}
+
+	private void IdleFinished() {
+		_attackRangeCollision.Disabled = false;
+		State = StateEnum.Chase;
 	}
 }
